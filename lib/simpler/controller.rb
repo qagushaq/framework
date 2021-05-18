54dff1f801d4ba9f8b2598f84a3e87a2d8b9b7b3
qagushaq <qwerty24164444@gmail.com>
2021-05-18 10:42:57 +0300

    views_ref

--- lib/simpler/controller.rb
+++ lib/simpler/controller.rb
@@ -2,12 +2,18 @@ require_relative 'view'
 module Simpler
   class Controller
 
+    attr_reader :name
+
     def initialize(env)
+      @name = extract_name
       @request = Rack::Request.new(env)
       @response = Rack::Response.new
     end
 
     def make_response(action)
+      @request.env['simpler.controller'] = self
+      @request.env['simpler.action'] = action
+
       set_default_headers
       write_response
 
@@ -15,6 +21,10 @@ module Simpler
     end
 
     private
+
+    def extract_name
+      self.class.name.match('(?<name>.+)Controller')[:name].downcase
+    end
 
     def set_default_headers
       @response['Content-Type'] = 'text/html'
--- lib/simpler/view.rb
+++ lib/simpler/view.rb
@@ -1,6 +1,8 @@
 module Simpler
   class View
 
+    VIEW_BASE_PATH = 'app/views'.freeze
+
     def initialize(env)
       @env = env
     end
@@ -11,8 +13,19 @@ module Simpler
 
     private
 
-    def template_path
-      Simpler.root.join('app/views/index.html')
+    def controller
+      @env['simpler.controller']
     end
+
+    def action
+      @env['simpler.action']
+    end
+
+    def template_path
+      path = [controller.name, action].join('/')
+
+      Simpler.root.join(VIEW_BASE_PATH,"#{path}.html")
+    end
+
   end
 end
